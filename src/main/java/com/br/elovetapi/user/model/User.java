bd68e75584aa3d3808c0aa8e0bf3cb01afdde3ca
Gustavo Pinheiro <mrpine19@gmail.com>
2026-09-09 00:21:59 -0300

    refector: atualizando as migrations com os novos campos criados

--- src/main/java/com/br/elovetapi/user/model/User.java
+++ src/main/java/com/br/elovetapi/user/model/User.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-@Table(name="users")
+@Table(name="elo_login")
 @Entity
 @Data
 @AllArgsConstructor
