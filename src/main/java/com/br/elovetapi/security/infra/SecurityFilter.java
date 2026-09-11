54957355165bf319461515972bbd249f5accf6d5
Gustavo Pinheiro <mrpine19@gmail.com>
2026-09-10 23:08:53 -0300

    feat: validação no securityfiler para validar se o login e user são nulor

--- src/main/java/com/br/elovetapi/security/infra/SecurityFilter.java
+++ src/main/java/com/br/elovetapi/security/infra/SecurityFilter.java
@@ -27,13 +27,22 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
-        if(token != null){
-            var login = tokenService.validateToken(token);
-            UserDetails user = userRepository.findByLogin(login);
 
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        if (token != null) {
+            var login = tokenService.validateToken(token);
+
+            if (login != null) {
+                UserDetails user = userRepository.findByLogin(login);
+
+                if (user != null) {
+                    var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                } else {
+                    SecurityContextHolder.clearContext();
+                }
+            }
         }
+
         filterChain.doFilter(request, response);
     }
 
